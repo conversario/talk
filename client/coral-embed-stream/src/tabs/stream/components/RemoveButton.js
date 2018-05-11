b5331b02467df8d306ada6b7322f2b1760eb0c55
Fabian Neumann <neumann@ferret-go.com>
2018-05-11 10:58:27 +0200

    remove-comment WIP

---
+++ client/coral-embed-stream/src/tabs/stream/components/RemoveButton.js
@@ -0,0 +1,236 @@
+import React, { Component } from 'react';
+
+import t from 'coral-framework/services/i18n';
+import { can } from 'coral-framework/services/perms';
+
+import { PopupMenu, Button } from 'coral-ui';
+import ClickOutside from 'coral-framework/components/ClickOutside';
+import cn from 'classnames';
+import styles from './RemoveButton.css';
+import * as REASONS from 'coral-framework/graphql/flagReasons';
+import PropTypes from 'prop-types';
+
+import { getErrorMessages, forEachError } from 'coral-framework/utils';
+
+// TODO: (kiwi) Need to adapt CSS classes post refactor to match the rest.
+const name = 'talk-plugin-flags';
+
+export default class RemoveButton extends Component {
+  state = {
+    showMenu: false,
+    itemType: '',
+    reason: '',
+    message: '',
+    step: 0,
+    localPost: null,
+  };
+
+  componentDidUpdate() {
+    if (this.popup) {
+      // this will be defined when the reporting popup is opened
+      this.popup.firstChild.style.top = `${this.RemoveButton.offsetTop -
+        this.popup.firstChild.clientHeight -
+        15}px`;
+    }
+  }
+
+  // When the "report" button is clicked expand the menu
+  onReportClick = () => {
+    const { currentUser } = this.props;
+    if (!currentUser) {
+      this.props.showSignInDialog();
+      return;
+    }
+    if (can(currentUser, 'INTERACT_WITH_COMMUNITY')) {
+      if (this.state.showMenu) {
+        this.closeMenu();
+      } else {
+        this.setState({ showMenu: true });
+      }
+    } else {
+      this.props.notify('error', t('error.NOT_AUTHORIZED'));
+    }
+  };
+
+  closeMenu = () => {
+    this.setState({
+      showMenu: false,
+      itemType: '',
+      reason: '',
+      message: '',
+      step: 0,
+    });
+  };
+
+  onPopupContinue = async () => {
+    const { postFlag, postDontAgree, id, author_id } = this.props;
+    const { itemType, reason, step, message } = this.state;
+    let failed = false;
+
+    switch (step) {
+      case 1: // *after* the last step
+        return this.closeMenu();
+      default:
+        throw new Error(`Unexpected step ${step}`);
+    }
+
+    // If itemType and reason are both set, post the action
+    if (step === 0) {
+      let action = {
+        item_id,
+        item_type: itemType,
+        message,
+      };
+
+      try {
+        const result = await postFlag({ ...action, reason });
+        if (itemType === 'COMMENTS') {
+          this.setState({ localPost: result.data.createFlag.flag.id });
+        }
+      } catch (errors) {
+        forEachError(errors, ({ error, msg }) => {
+          if (error.translation_key === 'ALREADY_EXISTS') {
+            msg = t('already_flagged_username');
+          }
+          this.props.notify('error', msg);
+        });
+        failed = true;
+      }
+    }
+
+    if (!failed) {
+      this.setState({ step: step + 1 });
+    }
+  };
+
+  onPopupOptionClick = sets => e => {
+    // If flagging a user, indicate that this is referencing the username rather than the bio
+    if (sets === 'itemType' && e.target.value === 'users') {
+      this.setState({ field: 'username' });
+    }
+
+    // Set itemType and field if they are defined in the popupMenu
+    const currentMenu = this.props.getPopupMenu[this.state.step]();
+    if (currentMenu.itemType) {
+      this.setState({ itemType: currentMenu.itemType });
+    }
+    if (currentMenu.field) {
+      this.setState({ field: currentMenu.field });
+    }
+
+    this.setState({ [sets]: e.target.value });
+  };
+
+  handleClickOutside = () => {
+    if (this.state.showMenu) {
+      this.closeMenu();
+    }
+  };
+
+  render() {
+    const { getPopupMenu, flaggedByCurrentUser } = this.props;
+    const { localPost } = this.state;
+    const flagged = flaggedByCurrentUser || localPost;
+    const popupMenu = getPopupMenu[this.state.step](this.state.itemType);
+
+    return (
+      <ClickOutside onClickOutside={this.handleClickOutside}>
+        <div className={`${name}-container`}>
+          <button
+            disabled={flagged}
+            ref={ref => (this.RemoveButton = ref)}
+            onClick={
+              !this.props.banned && !flaggedByCurrentUser && !localPost
+                ? this.onReportClick
+                : null
+            }
+            className={cn(
+              `${name}-button`,
+              {
+                [`${name}-button-flagged`]: flagged,
+                [styles.flaggedButton]: flagged,
+              },
+              styles.button
+            )}
+          >
+            {flagged ? (
+              <span className={`${name}-button-text`}>{t('reported')}</span>
+            ) : (
+              <span className={`${name}-button-text`}>{t('remove')}</span>
+            )}
+            <i
+              className={cn(`${name}-icon`, 'material-icons', styles.icon, {
+                flaggedIcon: flagged,
+                [styles.flaggedIcon]: flagged,
+              })}
+              aria-hidden={true}
+            >
+              delete
+            </i>
+          </button>
+          {this.state.showMenu && (
+            <div className={`${name}-popup`} ref={ref => (this.popup = ref)}>
+              <PopupMenu>
+                <div className={`${name}-popup-header`}>{popupMenu.header}</div>
+                {popupMenu.text && (
+                  <div className={`${name}-popup-text`}>{popupMenu.text}</div>
+                )}
+                {popupMenu.options && (
+                  <form className={`${name}-popup-form`}>
+                    {popupMenu.options.map(option => (
+                      <div key={option.val}>
+                        <input
+                          className={`${name}-popup-radio`}
+                          type="radio"
+                          id={option.val}
+                          checked={this.state[popupMenu.sets] === option.val}
+                          onClick={this.onPopupOptionClick(popupMenu.sets)}
+                          value={option.val}
+                        />
+                        <label
+                          htmlFor={option.val}
+                          className={`${name}-popup-radio-label`}
+                        >
+                          {option.text}
+                        </label>
+                        <br />
+                      </div>
+                    ))}
+                    {this.state.reason && (
+                      <div>
+                        <label
+                          htmlFor={'message'}
+                          className={`${name}-popup-textarea-label`}
+                        >
+                          {t('flag_reason')}
+                        </label>
+                      </div>
+                    )}
+                  </form>
+                )}
+                <div className={`${name}-popup-counter`}>
+                  {this.state.step + 1} of {getPopupMenu.length}
+                </div>
+                {popupMenu.button && (
+                  <Button
+                    className={`${name}-popup-button`}
+                    onClick={this.onPopupContinue}
+                  >
+                    {popupMenu.button}
+                  </Button>
+                )}
+              </PopupMenu>
+            </div>
+          )}
+        </div>
+      </ClickOutside>
+    );
+  }
+}
+
+RemoveButton.propTypes = {
+  currentUser: PropTypes.object,
+  getPopupMenu: PropTypes.array,
+  notify: PropTypes.func,
+  showSignInDialog: PropTypes.func,
+};
