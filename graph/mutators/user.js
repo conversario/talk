a5720a22e4b213556e6fc24ddab3e41a6c9ba4fc
Fabian Neumann <neumann@ferret-go.com>
2018-05-18 12:37:30 +0200

    Oops: re-add 2 accidentally removed lines.

--- graph/mutators/user.js
+++ graph/mutators/user.js
@@ -169,6 +169,9 @@ const delUser = async (ctx, id) => {
     }),
     Comment
   );
+
+  // Remove the user.
+  await user.remove();
 };
 
 const changeUserPassword = async (ctx, oldPassword, newPassword) => {
